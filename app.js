89e5c9e3e040113c721e3b3ac462b35788db711e
allan.pirillis <allan.p94@gmail.com>
2021-04-24 14:12:20 -0500

    added a style.css file that is copied to a dist folder to style the index.html file; moved the write and copy functionality to its own generateSite.js file that handles the logic and exports the functions to be used in app.js; created promises for the write and copy functions in order to be able to call the various functions asychroniously using promises;

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generatePage = require("./src/page-template.js");
+const { writeFile, copyFile } = require("./utils/generate-site.js");
 
 const mockData = {
   name: "Lernantino",
@@ -176,19 +177,51 @@ const promptProject = (portfolioData) => {
         return portfolioData;
       }
     })
-    .catch((eer) => console.log(err));
+    .catch((eer) => console.log(eer));
 };
+
+// promptUser()
+//   //   .then(promptProject)
+//   .then((portfolioData) => {
+//     // const pageHTML = generatePage(portfolioData);
+//     const pageHTML = generatePage(mockData);
+
+//     fs.writeFile("./dist/index.html", pageHTML, (err) => {
+//       if (err) {
+//         console.log(err);
+//         return;
+//       }
+//       console.log(
+//         "Page created! Check out index.html in this directory to see it!"
+//       );
+
+//       fs.copyFile("./src/style.css", "./dist/style.css", (err) => {
+//         if (err) {
+//           console.log(err);
+//           return;
+//         }
+//         console.log("Style sheet copied successfully!");
+//       });
+//     });
+//     //   console.log("Portfolio complete! Check out index.html to see the output!");
+//     // });
+//   });
 
 promptUser()
   //   .then(promptProject)
   .then((portfolioData) => {
-    // const pageHTML = generatePage(portfolioData);
-    const pageHTML = generatePage(mockData);
-
-    fs.writeFile("./index.html", pageHTML, (err) => {
-      if (err) throw new Error(err);
-    });
-
-    //   console.log("Portfolio complete! Check out index.html to see the output!");
-    // });
+    return generatePage(mockData);
+  })
+  .then((pageHTML) => {
+    return writeFile(pageHTML);
+  })
+  .then((writeFileResponse) => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then((copyFileResponse) => {
+    console.log(copyFileResponse);
+  })
+  .catch((err) => {
+    console.log(err);
   });
